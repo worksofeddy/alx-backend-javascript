7469b6e56df58bb7e32241fe683da0bf38d256d5
worksofeddy <worksofeddy@gmail.com>
2024-06-24 21:34:05 +0300

    javascript functions

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
+  }
+
+  return arr;
+}
--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
