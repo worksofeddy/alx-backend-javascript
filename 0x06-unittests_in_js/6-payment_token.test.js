975f2cf68baf76d68c462cfb250f25d9b9de4eb7
worksofeddy <worksofeddy@gmail.com>
2024-08-22 17:01:48 +0300

    unittest

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('should return a promise', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        assert.equal(res.data, 'Successful response from the API');
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
